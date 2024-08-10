359150a698cc8562efff05c546e862b13bdcad3d
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-10 15:16:15 +0900

    ✨:  user validation logic for email, password, and name

---
+++ app/schemas/users.py
@@ -0,0 +1,54 @@
+import re
+from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
+from typing import Any
+
+
+class UserCreate(BaseModel):
+    email: EmailStr = Field(..., max_length=100)
+    password: str = Field(..., min_length=8, max_length=20)
+    name: str = Field(..., min_length=2, max_length=50)
+
+    @staticmethod
+    def validate_field(validators: list[callable], value):
+        errors = []
+        for validator in validators:
+            try:
+                value = validator(value)
+            except ValueError as e:
+                errors.append(str(e))
+        if errors:
+            raise ValueError(errors)
+        return value
+
+    @field_validator("password")
+    def validate_password(cls, value: str) -> str:
+        return cls.validate_field(
+            [lambda x: x.strip(), cls.password_complexity], value  # 공백 제거
+        )
+
+    @staticmethod
+    def password_complexity(value: str) -> str:
+        if not (
+            any(c.islower() for c in value)
+            and any(c.isdigit() for c in value)
+            and any(c in "!@#$%^&*()_+" for c in value)
+        ):
+            raise ValueError(
+                "Password must include at least one lowercase letter, one number, and one special character"
+            )
+        return value
+
+    @field_validator("name")
+    def validate_name(cls, value: str) -> str:
+        return cls.validate_field(
+            [cls.name_characters, lambda x: " ".join(x.split())],
+            value,  # 중간 공백 압축
+        )
+
+    @staticmethod
+    def name_characters(value: str) -> str:
+        if not re.match(r"^[a-zA-Z\s\'-]+$", value):
+            raise ValueError(
+                "Name can only contain alphabets, spaces, hyphens, and apostrophes"
+            )
+        return value
