2f778478db96e53020de2aa9023842a61cc9c926
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-10 23:19:42 +0900

    refactor: Separate user validation logic into reusable functions

---
+++ app/exceptions/user_exceptions.py
@@ -0,0 +1,10 @@
+class DuplicateEmailError(Exception):
+    def __init__(self, message="Email already exists"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DatabaseError(Exception):
+    def __init__(self, message="An error occurred while accessing the database"):
+        self.message = message
+        super().__init__(self.message)
