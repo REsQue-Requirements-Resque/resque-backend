f874568cb1f3941ced258fb370c9133fdb3e04aa
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-10 12:34:03 +0900

    ✨ : Add async context manager for application lifespan & Feat logging

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.router import api_router
+from app.core.logging import logger
 
-app = FastAPI(title=settings.PROJECT_NAME)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Starting up the application")
+    yield
+    logger.info("Shutting down the application")
+
+
+print(settings.PROJECT_NAME)
+app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
