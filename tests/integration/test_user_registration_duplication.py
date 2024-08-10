5ed01a5b7b749e4d6b4349906ec197d1020abfc7
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-10 23:33:01 +0900

    Refactor user registration duplication tests

--- tests/integration/test_user_registration_duplication.py
+++ tests/integration/test_user_registration_duplication.py
@@ -40,7 +40,7 @@ class TestUserRegistrationDuplication:
             + self.passwords["valid"],  # 실제로는 해싱 함수를 사용해야 합니다
         )
         db_session.add(existing_user)
-        db_session.commit()
+        db_session.flush()
 
     def test_register_user_with_unique_email(self):
         data = self.get_valid_data()
@@ -69,29 +69,14 @@ class TestUserRegistrationDuplication:
         assert str(exc_info.value) == "Email already exists"
 
     def test_database_error_during_user_creation(self, db_session):
-        # 1. 유효한 사용자 데이터 준비
-        user_data = self.get_valid_data()
+        data = self.get_valid_data()
 
-        # 2. 데이터베이스 세션을 조작하여 OperationalError를 발생시킵니다.
-        def mock_commit():
-            raise DatabaseError("An error occurred while accessing the database")
+        def fake_commit():
+            raise Exception("Error")
 
-        original_commit = db_session.commit
-        db_session.commit = mock_commit
+        db_session.commit, original_commit = fake_commit, db_session.commit
 
-        # 3. 사용자 생성 시도 및 DatabaseError 확인
-        with pytest.raises(DatabaseError) as exc_info:
-            self.user_repository.create_user(UserCreate(**user_data))
-
-        # 4. 오류 메시지 확인
-        assert (
-            str(exc_info.value)
-            == "An unexpected error occurred: An error occurred while accessing the database"
-        )
-
-        # 5. 데이터베이스에 사용자가 생성되지 않았는지 확인
-        assert (
-            db_session.query(User).filter_by(email=user_data["email"]).first() is None
-        )
+        with pytest.raises(DatabaseError):
+            self.user_repository.create_user(UserCreate(**data))
 
         db_session.commit = original_commit
