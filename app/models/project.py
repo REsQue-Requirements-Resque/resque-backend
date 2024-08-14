3cce1538a41c15479e30a32d4224512acb4182ad
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-14 21:51:29 +0900

    feat: Add cascade delete for projects by founder

--- app/models/project.py
+++ app/models/project.py
@@ -1,19 +1,25 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 from datetime import datetime
+from sqlalchemy import UniqueConstraint
 
 
 class Project(Base):
     __tablename__ = "projects"
 
     id = Column(Integer, primary_key=True, index=True)
+    founder_id = Column(Integer, ForeignKey("users.id"), index=True)
     title = Column(String, index=True)
     description = Column(String, nullable=True)
-    founder_id = Column(Integer, ForeignKey("users.id"), index=True)
-    is_deleted = Column(Integer, default=0)
+
+    is_deleted = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow)
 
     # Relationship
-    founder = relationship("User", back_populates="projects")
+    founder = relationship("User", back_populates="projects", cascade="all, delete")
+
+    __table_args__ = (
+        UniqueConstraint("title", "founder_id", name="unique_project_title"),
+    )
