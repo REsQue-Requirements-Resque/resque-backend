c53f32d7f2c0641d49828912aea2f72ebe663438
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-14 19:40:20 +0900

    feat: Add Project model with necessary columns

---
+++ app/models/project.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    founder_id = Column(Integer, index=True)
+    is_deleted = Column(Integer, default=0)
