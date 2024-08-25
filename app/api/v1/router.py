ca67174f2430653f264e894d8a88f4a52aa3e3ec
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-26 04:11:34 +0900

    refactor: Update router imports in api/v1/router.py

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter
 from app.api.v1.endpoints.root import router as root_router
 from app.api.v1.endpoints.user import user_router
 from app.api.v1.endpoints.project import ProjectEndpoint
-from app.api.v1.endpoints.documents import DocumentEndpoint
-
+from app.api.v1.endpoints.document import DocumentEndpoint
+from app.api.v1.endpoints.requirement import RequirementEndpoint
 
 api_v1_router = APIRouter()
 
@@ -12,3 +12,4 @@ api_v1_router.include_router(root_router, tags=["root"], prefix="")
 api = api_v1_router.include_router(user_router, tags=["users"], prefix="/users")
 project_endpoint = ProjectEndpoint(api_v1_router)
 document_endpoint = DocumentEndpoint(api_v1_router)
+requirement_endpoint = RequirementEndpoint(api_v1_router)
