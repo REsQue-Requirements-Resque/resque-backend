a3bfb5ab8f335803d2e7200b2c270b1b4d8f382b
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-26 03:11:59 +0900

    refactor: Update main_router to use "/api/v1" prefix for API v1 routes

--- app/api/main_router.py
+++ app/api/main_router.py
@@ -1,6 +1,7 @@
 from app.api.v1.router import api_v1_router
 from fastapi import APIRouter
+from app.core.logging import logger
 
 main_router = APIRouter()
 
-main_router.include_router(api_v1_router, prefix="/v1")
+main_router.include_router(api_v1_router, prefix="/api/v1")
