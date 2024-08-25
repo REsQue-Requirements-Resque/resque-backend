29f52051b641edaf0f9261ea20f638222c0df4a4
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-26 03:27:59 +0900

    refactor: Update UserRepository to inherit from SoftDeleteBaseRepository
    The UserRepository class in the app.repositories.user_repository module has been updated to inherit from the SoftDeleteBaseRepository class. This change improves the codebase by utilizing the soft delete functionality provided by the base repository, allowing for more efficient and controlled deletion of user records.

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,46 +1,9 @@
 from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.repositories.soft_delete_base_repository import SoftDeleteBaseRepository
 
-from app.core.security import get_password_hash
-from app.exceptions.user_exceptions import DatabaseError, DuplicateEmailError
 from app.models.user import User
-from app.schemas.user import UserCreate
-from app.repositories.base_repository import BaseRepository
 
 
-class UserRepository(BaseRepository[User]):
-    def __init__(self, db_session: AsyncSession):
-        super().__init__(db_session, User)
-
-    async def create_user(self, user_data: UserCreate) -> User:
-        try:
-            if await self.check_user_existence(user_data.email):
-                raise DuplicateEmailError()
-
-            hashed_password = get_password_hash(user_data.password)
-            new_user_data = user_data.model_dump(exclude={"password"})
-            new_user_data["hashed_password"] = hashed_password
-
-            return await self.create(new_user_data)
-        except IntegrityError:
-            await self.db_session.rollback()
-            raise DatabaseError("An error occurred while accessing the database")
-        except DuplicateEmailError:
-            raise DuplicateEmailError("Email already exists")
-        except Exception as e:
-            await self.db_session.rollback()
-            raise DatabaseError(f"An unexpected error occurred: {str(e)}")
-
-    async def check_user_existence(self, email: str) -> bool:
-        email = email.lower()
-        user = await self.get_user_by_email(email)
-        return user is not None
-
-    async def get_user_by_email(self, email: str) -> User:
-        stmt = select(self.model).filter(self.model.email == email.lower())
-        result = await self.db_session.execute(stmt)
-        return result.scalar_one_or_none()
-
-    async def update_user(self, user_id: int, user_data: dict) -> User:
-        return await self.update(user_id, user_data)
+class UserRepository(SoftDeleteBaseRepository):
+    _model_class = User
