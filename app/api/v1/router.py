35bf05c953d5cd4a6da1c0db424dd0bc52e1eb76
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-26 03:29:35 +0900

    refactor: Add DocumentEndpoint for CRUD operations on documents

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from app.api.v1.endpoints.root import router as root_router
 from app.api.v1.endpoints.user import user_router
 from app.api.v1.endpoints.project import ProjectEndpoint
-from app.core.logging import logger
+from app.api.v1.endpoints.documents import DocumentEndpoint
+
 
 api_v1_router = APIRouter()
 
 api_v1_router.include_router(root_router, tags=["root"], prefix="")
 api = api_v1_router.include_router(user_router, tags=["users"], prefix="/users")
 project_endpoint = ProjectEndpoint(api_v1_router)
+document_endpoint = DocumentEndpoint(api_v1_router)
