6fd8f21a9537a6368d556b1f9e4d93ce9600a009
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-26 00:26:24 +0900

    refactor: Update main_router to include API v1 router
    This commit updates the main_router module to include the API v1 router, allowing access to API v1 routes using the "/v1" prefix. This change improves the organization and structure of the codebase.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.api.v1.router import api_router
+from app.api.main_router import main_router
 from app.core.config import settings
 from app.core.logging import logger
 
@@ -17,4 +17,7 @@ async def lifespan(app: FastAPI):
 print(settings.PROJECT_NAME)
 app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(main_router)
+
+for route in app.routes:
+    logger.info(f"Route: {route.path}, methods: {route.methods}")
