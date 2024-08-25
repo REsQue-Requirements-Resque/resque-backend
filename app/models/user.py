d8717b9db526f79466f0aac0490fc713e03d23b8
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-26 03:27:38 +0900

    refactor: Add username column to User model
    The User model in the app.models.user module has been updated to include a new username column. This change adds the ability to store and retrieve usernames for users in the system.

--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
+from app.models.mixins.soft_delete_mixin import SoftDeleteMixin
 from app.db.base import Base
 
 
-class User(Base):
+class User(Base, SoftDeleteMixin):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     name = Column(String)
     hashed_password = Column(String)
-
-    projects = relationship(
-        "Project", back_populates="founder", cascade="all, delete-orphan"
-    )
